8aa49a80df14c0d11b3bb0d164185ef2fd4b7113
fcva <fcva@gmail.com>
2025-01-28 11:18:26 -0500

    actualizando rutas api.php

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,10 @@
 <?php
 
-// use App\Http\Controllers\Administrator\AdministratorController;
-
 use App\Http\Controllers\Administrator\AdministratorController;
 use App\Http\Controllers\Estudiante\EstudianteController;
 use App\Http\Controllers\Role\RoleController;
 use App\Http\Controllers\Cliente\ClienteController;
+use App\Http\Controllers\Producto\ProductoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,12 +18,11 @@ Route::resource('/roles', RoleController::class);
 // By Danniels
 Route::resource('/estudiantes', EstudianteController::class);
 
-//By Kenny
+// By Kenny
 Route::resource('/clientes', ClienteController::class);
 
-// Route::resource('/vehiculos', VehiculoController::class);
-
+// By Jeferson
+Route::resource('/productos', ProductoController::class);
 
 // By Adler
 Route::resource('/administrator', AdministratorController::class);
-//-----
